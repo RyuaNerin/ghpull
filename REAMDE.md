eb82e8de26bb6aee3aba33bd34f179bac408e5dd
RyuaNerin <admin@ryuar.in>
2020-08-16 03:16:30 +0900

    first commit

---
+++ REAMDE.md
@@ -0,0 +1,70 @@
+# ghpull
+
+Auto git-pull by GitHub WebHook
+
+## Installation
+
+- Environment in this case
+    |Description|Value|
+    |-|-|
+    |GitHub Repository|`https://github.com/RyuaNerin/portfolio`|
+    |GitHub WebHook Payload URL|`https://ryuar.in/__push`|
+    |GitHub WebHook Secret|`1234567890`|
+    |Local Repository Directory|`/srv/http/ryuar.in/_default`|
+    |SSH Key|`/home/ghpull/.ssh/id_ed25519`|
+    |HTTP Server Binding|tcp `:8081`|
+
+1. Config Github Actions
+    1. **Repository Page** -> **Settings** -> **Webhooks**
+    1. Click **Add webhook**
+    1. Input `https://ryuar.in/__push` in **Payload URL**
+    1. Input `1234567890` in **Secret**
+    1. Select **Just the push event.**
+    1. Click `Add Webhook.`
+
+1. Clone target repository.
+
+    ```shell
+    > git clone https://github.com/RyuaNerin/portfolio -o /srv/http/ryuar.in/_default
+    ```
+
+1. Clone this repository, then build package
+
+    ```shell
+    > git clone git@github.com:RyuaNerin/ghpull.git
+    > cd ghpull
+    > go build -v
+    ```
+
+1. Copy binary.
+
+    ```shell
+    > cp ghpull /usr/local/bin/ghpull
+    ```
+
+1. Run ghpull and test.
+
+    ```shell
+    > /usr/local/bin/ghpull -key "/home/ghpull/.ssh/id_ed25519" -unix "/run/ghpull/ryuar.in.sock" -path "/__push" -dir "/srv/http/ryuar.in/_default" -secret "1234567890"
+    ```
+
+1. To Use systemd.
+
+    ```shell
+    > sudo cp ghpull.service /etc/systemd/system/
+    > systemctl enable ghpull.service
+    > systemctl start ghpull.service
+    ```
+
+1. Edit Nginx Configure.
+
+    ```shell
+    > vi /etc/nginx/conf.d/ryuar.in.conf
+    ```
+
+    ```nginx
+    location /__push {
+        include fastcgi_params;
+        fastcgi_pass unix:/run/ghpull/ryuar.in.sock;
+    }
+    ```
---
+++ ghpull.go
@@ -0,0 +1,269 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	golog "log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"reflect"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+	"unsafe"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	secret []byte
+	path   string
+	key    string
+	dir    string
+
+	poolTimeout int
+
+	gitPullLock sync.Mutex
+	gitPullErr  bytes.Buffer
+
+	log    *golog.Logger
+	logErr *golog.Logger
+)
+
+func main() {
+	log = golog.New(os.Stdout, "", golog.LstdFlags)
+	logErr = golog.New(os.Stderr, "", golog.LstdFlags)
+
+	argSecret := flag.String("secret", "", "WebHook Secret")
+	flag.StringVar(&dir, "dir", "", "Directory of repository")
+	flag.StringVar(&path, "path", "/push", "WebHook path")
+	flag.StringVar(&key, "key", "", "Private key to use for git commands")
+
+	argUnix := flag.String("unix", "", "Bind to UNIX Socket")
+	argUnixPerm := flag.Int("unix-perm", 777, "UNIX Socket permissions")
+	argBind := flag.String("bind", "localhost:8081", "Bind to tcp")
+
+	flag.IntVar(&poolTimeout, "timeout", 30000, "")
+
+	flag.Parse()
+
+	secret = []byte(*argSecret)
+
+	var err error
+	if dir == "" {
+		log.Println("dir is empty")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if _, err = os.Open(dir); err != nil {
+		log.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if key == "" {
+		log.Println("key is empty")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fs, err := os.Open(key)
+	if err != nil {
+		log.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
+	fs.Close()
+
+	var l net.Listener
+
+	if *argUnix != "" {
+		os.Remove(*argUnix)
+
+		l, err = net.Listen("unix", *argUnix)
+		if err != nil {
+			log.Fatal(errors.WithStack(err))
+		}
+		defer l.Close()
+
+		log.Println("Listen unix", *argUnix)
+
+		defer os.Remove(*argUnix)
+
+		perm := os.FileMode((*argUnixPerm / 100 * 16) + (*argUnixPerm / 10 % 10 * 8) + (*argUnixPerm % 10))
+		err = os.Chmod(*argUnix, perm)
+		if err != nil {
+			log.Fatal(errors.WithStack(err))
+		}
+	} else {
+		l, err = net.Listen("tcp", *argBind)
+		if err != nil {
+			log.Fatal(errors.WithStack(err))
+		}
+		defer l.Close()
+
+		log.Println("Listen tcp", *argBind)
+	}
+
+	server := http.Server{
+		ErrorLog: logErr,
+		Handler:  http.HandlerFunc(handler),
+	}
+
+	go func() {
+		err := server.Serve(l)
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(errors.WithStack(err))
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	<-sig
+
+	server.Shutdown(context.TODO())
+}
+
+func s2b(s string) (b []byte) {
+	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	bh.Data = sh.Data
+	bh.Len = sh.Len
+	bh.Cap = sh.Len
+	return b
+}
+func b2s(b []byte) (s string) {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost || !strings.HasPrefix(r.RequestURI, path) {
+		log.Println("bad request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	XHubSignature := r.Header.Get("x-hub-signature")
+	if len(XHubSignature) != 45 || !strings.HasPrefix(XHubSignature, "sha1=") {
+		log.Println("invalid x-hub-signature")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	h := hmac.New(sha1.New, secret)
+	_, err := io.Copy(h, r.Body)
+	if err != nil && err != io.EOF {
+		logErr.Println(err)
+		return
+	}
+
+	// sha1=
+	var buf [40]byte
+	hex.Encode(buf[:], h.Sum(nil))
+
+	if !bytes.EqualFold(buf[:], s2b(XHubSignature)[5:]) {
+		log.Println("invalid key")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	go func() {
+		gitPullLock.Lock()
+		defer gitPullLock.Unlock()
+
+		cmd := exec.Cmd{
+			Dir:  dir,
+			Path: "/bin/bash",
+			Args: []string{
+				"/bin/bash", "-c", fmt.Sprintf("/usr/bin/ssh-add -q \"%s\"; /usr/bin/git pull", key),
+			},
+		}
+
+		cmdErr, err := cmd.StderrPipe()
+		if cmdErr != nil {
+			defer cmdErr.Close()
+		}
+
+		err = cmd.Start()
+		if err != nil {
+			logErr.Println(errors.WithStack(err))
+			return
+		}
+
+		log.Println("git-pull started")
+
+		chCmdErrDone := make(chan struct{}, 1)
+		if cmdErr != nil {
+			go func() {
+				_, _ = io.Copy(&gitPullErr, cmdErr)
+				chCmdErrDone <- struct{}{}
+			}()
+		} else {
+			chCmdErrDone <- struct{}{}
+		}
+
+		ch := make(chan error, 1)
+		go func() {
+			ch <- cmd.Wait()
+		}()
+
+		select {
+		case <-time.After(time.Duration(poolTimeout) * time.Millisecond):
+			printGitErr(chCmdErrDone)
+
+			log.Println("git-pull timeout.")
+			err = cmd.Process.Kill()
+			if err != nil {
+				logErr.Println(err)
+			}
+
+		case err = <-ch:
+			printGitErr(chCmdErrDone)
+
+			log.Println("git-pull done.")
+			if gitPullErr.Len() > 0 {
+				logErr.Println(gitPullErr.Bytes())
+			}
+
+			if err != nil {
+				logErr.Println(err)
+			}
+		}
+	}()
+}
+
+func printGitErr(chCmdErrDone <-chan struct{}) {
+	<-chCmdErrDone
+	defer gitPullErr.Reset()
+
+	firstLine := true
+
+	br := bufio.NewReader(&gitPullErr)
+	for {
+		line, _, err := br.ReadLine()
+		if err != nil {
+			break
+		}
+
+		if firstLine {
+			logErr.Println(("--------------------------------------------------"))
+			logErr.Println("git-pull stderr:")
+			defer logErr.Println("--------------------------------------------------")
+			firstLine = false
+		}
+
+		logErr.Println(b2s(line))
+	}
+}
